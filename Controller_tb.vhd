f62e3682384f3bf12efdcbce66760c2d491b4d99
icerui11 <104990576+icerui11@users.noreply.github.com>
2024-03-11 17:39:08 +0100

    将ce信号和address分开
    因为dut中 process以ce为sensitiv list

--- Controller_tb.vhd
+++ Controller_tb.vhd
@@ -1,8 +1,23 @@
+--============================================================================--
+-- Design unit  : Rom_controller module
+--
+-- File name    : Brom_tb.vhd
+--
+-- Purpose      : read data from rom
+--
+-- Note         :
+--
+-- Library      : shyloc_utils
+--
+-- Author       : Rui Yin
+--
+-- Instantiates : 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-use work.ROM_Package.all;
+--use work.ROM_Package.all;
 
 entity Brom_tb is
 end Brom_tb;
@@ -15,7 +30,7 @@ architecture tb of Brom_tb is
   
   -- Signals for testbench
   signal CE : std_logic := '0';
-  signal rom_addr_tb: std_logic_vector (addr-1 downto 0);
+  signal rom_addr: std_logic_vector (addr-1 downto 0);
   signal rom_data_o_tb : std_logic_vector(width-1 downto 0);
   signal DataIn_NewValid_tb : std_logic := '0';
 
@@ -44,30 +59,25 @@ begin
     )
     port map (
       CE => CE,
-      rom_addr => rom_addr_tb,
+      rom_addr => rom_addr,
       rom_data_o => rom_data_o_tb,
       DataIn_NewValid => DataIn_NewValid_tb
     );
+process
+ begin
+    CE <= '0';
+    wait for 10 ns; 
+    CE <= '1';
+    wait for 10 ns;
+end process;
+  stimulus : process
+  begin
 
-    test_process : process
-    begin
-	wait for 40ns;
-	CE <= '0';
-	wait for 20ns;
-	CE <= '1';
-        wait;
-    end process;
-	
- process(CE)
-begin
-        if (CE = '0') then
-	    rom_addr_tb <= (others => '0');
-        
-         elsif CE = '1' then
-                    rom_addr_tb <= std_logic_vector(unsigned(rom_addr_tb) + 1);                        -- increment the address
-           
-        end if;
-    end process;
+    for i in 0 to depth-1 loop
+      wait for 20 ns; 
+      rom_addr <= std_logic_vector(to_unsigned(i, rom_addr'length)); 
+    end loop;
   
-end tb;
+  end process stimulus;
 
+end tb;
